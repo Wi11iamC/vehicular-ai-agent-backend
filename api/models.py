dbf05b9372353a8476dfcb963b4e52fc405af398
EDDIE <eddie.musashi1@gmail.com>
2023-06-19 11:38:47 -0400

    added semantic segmentation camera model

--- api/models.py
+++ api/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class rgb_sensor(models.Model):
     image_url=models.ImageField(upload_to='rgb_images')
+
+class semantic_segmentation_camera(models.Model):
+    image_url = models.ImageField(upload_to = "semantic_segmentation_images")
+
